a05b6efe84c37bb237f79419d5cde1afe9a0924c
Andy Smith <DrewAndrewSmith@netscape.net>
2023-12-20 16:27:22 +0000

    Changing error message for Grubbs

--- outliers_grubbs_test.py
+++ outliers_grubbs_test.py
@@ -2,23 +2,23 @@ from scipy.stats import t
 import numpy as np
 
 def outliers_grubbs_test(data_array, label, measure, alpha = 0.05, test_type = 'two-sided'):
-    
+
     # Input validation checks
-    if not isinstance(data_array, list) or len(data_array) < 7:
-        raise ValueError('data_array must be a list containing seven or more values.')
+    if not isinstance(data_array, list) or len(data_array) == 0 or not isinstance(data_array[0], dict):
+        raise ValueError('The first parameter requires a list of dictionaries.')
     
     if not isinstance(label, str):
-        raise ValueError('label must be a string.')
+        raise ValueError('The second parameter requires a string.')
     
     if not isinstance(measure, str):
-        raise ValueError('measure must be a string.')
+        raise ValueError('The third parameter requires a string.')
     
-    if not isinstance(alpha, (int, float)) or alpha < 0 or alpha > 1:
-        raise ValueError('alpha must be a number between 0 and 1.')
+    if not isinstance(alpha, (int, float)) or alpha <= 0 or alpha >= 1:
+        raise ValueError('The fourth parameter requires a number betwwen 0 and 1.')
     
     valid_test_types = ['two-sided', 'left-sided', 'right-sided']
     if test_type not in valid_test_types:
-        raise ValueError('Invalid test_type. Valid inputs are: "two-sided", "left-sided", "right-sided."')
+        raise ValueError("The fifth parameter requires 'two-sided', 'left-sided', or 'right-sided.'")
 
     # Get sample mean and standard deviation
     mean = np.mean([data_point[measure] for data_point in data_array])
